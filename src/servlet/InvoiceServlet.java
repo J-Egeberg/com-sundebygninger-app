1fe1d4f6e0e6645233b578cec6a1f5d329dec52b
Schelde Jonas <scheldejonas@gmail.com>
2016-12-02 14:35:27 +0100

    Updating with libraries

--- src/servlet/InvoiceServlet.java
+++ src/servlet/InvoiceServlet.java
@@ -1,19 +1,22 @@
 package servlet;
 
-import java.io.IOException;
-import javax.servlet.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by jensegebergrasmussen on 30/11/16.
  */
 @WebServlet( name = "InvoiceList", urlPatterns = { "/invoices/index", "/invoices/details", "/invoices/add" } )
-public class InvoiceServlet extends HttpServlet{
+public class InvoiceServlet extends HttpServlet {
 
     @Override
-    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         processGet( request, response );
     }
 
@@ -44,5 +47,4 @@ public class InvoiceServlet extends HttpServlet{
     public String getServletInfo() {
         return "En servlet for faktura";
     }
-
 }
