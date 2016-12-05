bfa2a225c1f0a7dc3c5eda64699e1bcd1349832d
Schelde Jonas <scheldejonas@gmail.com>
2016-12-05 17:51:51 +0100

    Upgrading DBConnection with test to Main App

--- src/data/DBConnection.java
+++ src/data/DBConnection.java
@@ -6,17 +6,29 @@ import java.sql.SQLException;
 
 public final class DBConnection {
 
-    public static String USER = "java";
-    public static String USERPW = "password";
-    public static String DBNAME = "sundebygninger";
-    public static String HOST = "localhost";
+    // Standard localhost DB for starting app in isolated environment
+    public static String USER_STD = "java";
+    public static String USERPW_STD = "password";
+    public static String DBNAME_STD = "sundebygninger";
+    public static String HOST_STD = "localhost";
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+    // Jonas localhost DB for testing with Main App
+    public static String USER_JS_MAC = "root";
+    public static String USERPW_JS_MAC = "tEb6M>CAEuua1";
+    public static String DBNAME_JS_MAC = "app";
+    public static String HOST_JS_MAC = "localhost";
 
-        String url = String.format("jdbc:mysql://%s:3306/%s?useSSL=false", HOST, DBNAME);
+    public static Connection getConnection(String profile) throws SQLException, ClassNotFoundException {
 
-        Class.forName("com.mysql.jdbc.Driver");
-
-        return DriverManager.getConnection(url, USER, USERPW);
+        if (profile.equals("test-js-mac")) {
+            String url = String.format("jdbc:mysql://%s:3306/%s?useSSL=false", HOST_JS_MAC, DBNAME_JS_MAC);
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(url, USER_JS_MAC, USERPW_JS_MAC);
+        }
+        else {
+            String url = String.format("jdbc:mysql://%s:3306/%s?useSSL=false", HOST_STD, DBNAME_STD);
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(url, USER_STD, USERPW_STD);
+        }
     }
 }
